1ebccbefb430e08b90975ebf3301effbc1ec0880
Matthew Stronge <matthew.stronge@gmail.com>
2014-07-30 00:46:43 +0100

    Friendly forwarding and correct user requirement

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,9 +22,26 @@ module SessionsHelper
 		self.current_user = nil
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+
     def deny_access
       redirect_to login_path, :flash => { :alert => "Please login to access this page."}
-    end	
+    end
+
+    def store_location
+		session[:return_to] = request.fullpath
+    end
+
+    def redirect_back_or(default)
+    	redirect_to(session[:return_to] || default)
+    	clear_return_to
+    end
+
+    def clear_return_to
+    	session[:return_to] = nil
+    end
 
 	private
 
