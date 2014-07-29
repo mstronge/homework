c06343d8c1ef866230ac8c2c00944f1121685d09
Matthew Stronge <matthew.stronge@gmail.com>
2014-07-30 00:30:50 +0100

    Protecting edit and update with before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_filter :authenticate, :only => [:edit, :update, :show]
 
   def show
   	@user = User.find(params[:id])
@@ -42,5 +42,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+
+    def authenticate
+      
+      deny_access unless signed_in?
+    end
 
 end
