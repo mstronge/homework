8f11ff9c7e1e027a2e821ab0c28a38bb930962d3
Alexander Zakrevsky <multik888@gmail.com>
2014-08-20 13:48:33 +0300

    admin=true

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,30 +15,30 @@ class UsersController < ApplicationController
   end
 
   def new
-  	if signed_in?
-  		redirect_to root_path
-  		flash[:success] = "You are already signed in."
-  	end
+    if signed_in?
+      redirect_to root_path
+  	flash[:success] = "You are already signed in."
+    end
 
-  	@title = "Sign up"
-  	@user = User.new
+    @title = "Sign up"
+    @user = User.new
   end
 
   def create
-  	@user = User.new(params[:user])
-  	if @user.save
-  		flash[:success] = "Thank you for signing up, your teacher will be in touch."
-  		redirect_to @user
-  	else
-  	@title = "Sign up"
-  	render 'new'
-  	end
+    @user = User.new(params[:user])
+    @user.admin = true
+    if @user.save
+      flash[:success] = "Thank you for signing up, your teacher will be in touch."
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
   def edit
     @user = User.find(params[:id])
     @title = "Edit account details"
-
   end
 
   def update
