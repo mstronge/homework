0f58b30cab9e528e2d35d7d0052c8341e43f1517
Matthew Stronge <matthew.stronge@gmail.com>
2014-09-22 19:58:39 +0100

    Added postmark support for mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => "smtp.postmarkapp.com", :port => 2525 }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
